35422cab8e513ab3b15231744d3224d6ec809f44
Justin Cho <jcho088@yahoo.com>
2016-05-23 18:11:54 -0700

    added posting method, need to test

---
+++ Salert/app/src/main/java/com/rektgg/salert/DealPost.java
@@ -0,0 +1,31 @@
+package com.rektgg.salert;
+
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+/**
+ * Created by Justin on 5/23/2016.
+ */
+public class DealPost extends ParseObject {
+
+    public String getText() {
+        return getString("text");
+    }
+
+    public void setText(String value) {
+        put("text", value);
+    }
+
+    public ParseUser getUser() {
+        return getParseUser("user");
+    }
+
+    public void setUser(ParseUser value) {
+        put("user", value);
+    }
+
+    public static ParseQuery<DealPost> getQuery() {
+        return ParseQuery.getQuery(DealPost.class);
+    }
+}
--- Salert/app/src/main/java/com/rektgg/salert/PostActivity.java
+++ Salert/app/src/main/java/com/rektgg/salert/PostActivity.java
@@ -1,13 +1,69 @@
 package com.rektgg.salert;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class PostActivity extends AppCompatActivity {
+
+    private Button submitButton;
+    private EditText postEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        submitButton = (Button) findViewById(R.id.bt_submit);
+        submitButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                post();
+            }
+        });
+
+//        Intent intent = new Intent(PostActivity.this, ShopProfileActivity.class);
+//        intent.putExtra(Application.INTENT_EXTRA_LOCATION, myLoc);
+//        startActivity(intent);
+    }
+
+    private void post () {
+
+        String text = postEditText.getText().toString().trim();
+
+        // Set up a progress dialog
+        final ProgressDialog dialog = new ProgressDialog(PostActivity.this);
+        dialog.setMessage(getString(R.string.progress_post));
+        dialog.show();
+
+        DealPost post = new DealPost();
+
+        post.setText(text);
+        post.setUser(ParseUser.getCurrentUser());
+        ParseACL acl = new ParseACL();
+
+        // Give public read access
+        acl.setPublicReadAccess(true);
+        post.setACL(acl);
+
+        // Save the post
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+
     }
 }
