c05b34ef830f3663d9f593aa87e5cc14ee2eed37
Steve <steveyg88@gmail.com>
2016-07-26 13:07:13 -0700

    add API key

---
+++ Salert/app/src/main/java/com/rektgg/salert/TestAPIActivity.java
@@ -0,0 +1,40 @@
+package com.rektgg.salert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//public class TestAPIActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_test_api);
+//    }
+//}
+
+//import com.google.android.gms.common.api.GoogleApiClient;
+//import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+//import com.google.android.gms.location.places.Places;
+//
+//import android.support.v4.app.FragmentActivity;
+//
+//public class TestAPIActivity extends AppCompatActivity
+//        implements OnConnectionFailedListener {
+//    private GoogleApiClient mGoogleApiClient;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        mGoogleApiClient = new GoogleApiClient
+//                .Builder(this)
+//                .addApi(Places.GEO_DATA_API)
+//                .addApi(Places.PLACE_DETECTION_API)
+//                .enableAutoManage(this, this)
+//                .build();
+//    }
+//
+//    // TODO: Please implement GoogleApiClient.OnConnectionFailedListener to
+//    // handle connection failures.
+//}
