e11e14658b3b29e7a99067159f50e540395f379e
Justin <justmyung@gmail.com>
2016-07-26 17:47:13 -0700

    need to get current location

--- Salert/app/src/main/java/com/rektgg/salert/MainActivity.java
+++ Salert/app/src/main/java/com/rektgg/salert/MainActivity.java
@@ -1,10 +1,16 @@
 package com.rektgg.salert;
 
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.model.LatLng;
 
 import android.util.Log;
@@ -20,8 +26,8 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentActivity;
 import android.content.pm.PackageManager;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.location.LocationServices;
@@ -29,101 +35,25 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.parse.FindCallback;
 import com.parse.ParseUser;
-import com.parse.ParseGeoPoint;
 
-public class MainActivity extends FragmentActivity implements LocationListener,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
+    private static final String LOG_TAG = "MainActivity";
+    private static final int GOOGLE_API_CLIENT_ID = 0;
     private LocationRequest locRequest;
     private GoogleApiClient locClient;
-
-    /*
-   * Define a request code to send to Google Play services This code is returned in
-   * Activity.onActivityResult
-   */
-    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-
-    /*
-     * Constants for location update parameters
-     */
-    // Milliseconds per second
-    private static final int MILLISECONDS_PER_SECOND = 1000;
-
-    // The update interval
-    private static final int UPDATE_INTERVAL_IN_SECONDS = 5;
-
-    // A fast interval ceiling
-    private static final int FAST_CEILING_IN_SECONDS = 1;
-
-    // Update interval in milliseconds
-    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = MILLISECONDS_PER_SECOND
-            * UPDATE_INTERVAL_IN_SECONDS;
-
-    // A fast ceiling of update intervals, used when the app is visible
-    private static final long FAST_INTERVAL_CEILING_IN_MILLISECONDS = MILLISECONDS_PER_SECOND
-            * FAST_CEILING_IN_SECONDS;
-
-    /*
-     * Constants for handling location results
-     */
-    // Conversion from feet to meters
-    private static final float METERS_PER_FEET = 0.3048f;
-
-    // Conversion from kilometers to meters
-    private static final int METERS_PER_KILOMETER = 1000;
-
-    // Initial offset for calculating the map bounds
-    private static final double OFFSET_CALCULATION_INIT_DIFF = 1.0;
-
-    // Accuracy for calculating the map bounds
-    private static final float OFFSET_CALCULATION_ACCURACY = 0.01f;
-
-    // Maximum results returned from a Parse query
-    private static final int MAX_POST_SEARCH_RESULTS = 20;
-
-    // Maximum post search radius for map in kilometers
-    private static final int MAX_POST_SEARCH_DISTANCE = 100;
-
     private Location lastLocation;
     private Location currentLocation;
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private static final int PERMISSION_REQUEST_CODE = 100;
 
-    public static final String API = "AIzaSyBBpDwSSigz71S5ITu7v4acH3n6RcgNZik";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_main);
-
-        // Create a new global location parameters object
-        locRequest = LocationRequest.create();
-
-        // Set the update interval
-        locRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
-
-        // Use high accuracy
-        locRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        // Set the interval ceiling to one minute
-        locRequest.setFastestInterval(FAST_INTERVAL_CEILING_IN_MILLISECONDS);
-
-        // Create a new location client, using the enclosing class to handle callbacks.
-        locClient = new GoogleApiClient.Builder(this)
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-
-        if(!(locClient == null)) {
-            Log.d("tag", "loc client not null");
-        }
-        else {
-            Log.d("tag", "loc client is null");
-        }
 
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
@@ -145,6 +75,12 @@ public class MainActivity extends FragmentActivity implements LocationListener,
             signupButton.setVisibility(View.GONE);
             loginButton.setVisibility(View.GONE);
         }
+
+        locClient = new GoogleApiClient.Builder(MainActivity.this)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, GOOGLE_API_CLIENT_ID, this)
+                .build();
+
         // Sign up button click handler
 
         signupButton.setOnClickListener(new OnClickListener() {
@@ -165,6 +101,18 @@ public class MainActivity extends FragmentActivity implements LocationListener,
 
         gpsButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
+                if (locClient.isConnected()) {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                                PERMISSION_REQUEST_CODE);
+                    } else {
+                        callPlaceDetectionApi();
+                    }
+
+                }
                 startActivity(new Intent(MainActivity.this, ShopProfileActivity.class));
             }
         });
@@ -207,199 +155,54 @@ public class MainActivity extends FragmentActivity implements LocationListener,
         return true;
     }
 
- /*
-  * Called when the Activity is no longer visible at all. Stop updates and disconnect.
-  */
     @Override
-    public void onStop() {
-        // If the client is connected
-        if (locClient.isConnected()) {
-            stopPeriodicUpdates();
-        }
-
-        // After disconnect() is called, the client is considered "dead".
-        locClient.disconnect();
-
-        super.onStop();
-    }
-
-    /*
-     * Called when the Activity is restarted, even before it becomes visible.
-     */
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // Connect to the location services client
-        locClient.connect();
-    }
-
-    /*
-     * Called when the Activity is resumed. Updates the view.
-     */
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        Application.getConfigHelper().fetchConfigIfNeeded();
-
-        // Get the latest search distance preference
-//        radius = Application.getSearchDistance();
-        // Checks the last saved location to show cached data if it's available
-        if (lastLocation != null) {
-            LatLng myLatLng = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
-            // If the search distance preference has been changed, move
-            // map to new bounds.
-//            if (lastRadius != radius) {
-//                updateZoom(myLatLng);
-//            }
-//            // Update the circle map
-//            updateCircle(myLatLng);
-        }
-//        // Save the current radius
-//        lastRadius = radius;
-//        // Query for the latest data to update the views.
-//        doMapQuery();
-//        doListQuery();
-    }
-
-    private boolean servicesConnected() {
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-
-        if (ConnectionResult.SUCCESS == resultCode) {
-            return true;
-        } else {
-            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0);
-            if (dialog != null) {
-                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
-                errorFragment.setDialog(dialog);
-                errorFragment.show(getSupportFragmentManager(), Application.APPTAG);
-            }
-            return false;
-        }
-    }
-
-    /*
-      * Called by Location Services when the request to connect the client finishes successfully. At
-      * this point, you can request the current location or start periodic updates
-      */
-    public void onConnected(Bundle bundle) {
-        if (Application.APPDEBUG) {
-            Log.d("Connected", Application.APPTAG);
-        }
-        Log.d("test", "inside onConnected");
-
-        currentLocation = getLocation();
-
-        Log.d("test", "current Location = " + getLocation());
-
-        startPeriodicUpdates();
-    }
-
-    /*
-     * Called by Location Services if the connection to the location client drops because of an error.
-     */
-    public void onDisconnected() {
-        if (Application.APPDEBUG) {
-            Log.d("Disconnected", Application.APPTAG);
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.i(Application.APPTAG, "GoogleApiClient connection has been suspend");
-    }
-
-    /*
-     * Called by Location Services if the attempt to Location Services fails.
-     */
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        // Google Play services can resolve some errors it detects. If the error has a resolution, try
-        // sending an Intent to start a Google Play services activity that can resolve error.
-        if (connectionResult.hasResolution()) {
-            try {
+        Log.e(LOG_TAG, "Google Places API connection failed with error code: "
+                + connectionResult.getErrorCode());
 
-                // Start an Activity that tries to resolve the error
-                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+        Toast.makeText(this,
+                "Google Places API connection failed with error code:" +
+                        connectionResult.getErrorCode(),
+                Toast.LENGTH_LONG).show();
+    }
 
-            } catch (IntentSender.SendIntentException e) {
-
-                if (Application.APPDEBUG) {
-                    // Thrown if Google Play services canceled the original PendingIntent
-                    Log.d(Application.APPTAG, "An error occurred when connecting to location services.", e);
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    callPlaceDetectionApi();
                 }
+                break;
+        }
+    }
+
+
+    private void callPlaceDetectionApi() throws SecurityException {
+        Log.d("tag", "call placedetect");
+        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+                .getCurrentPlace(locClient, null);
+        Log.d("result", "RESULT PLACES = " + result);
+        result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+            @Override
+            public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+                Log.d("tag", "call onResult");
+                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                    Log.i(LOG_TAG, String.format("Place '%s' with " +
+                                    "likelihood: %g",
+                            placeLikelihood.getPlace().getName(),
+                            placeLikelihood.getLikelihood()));
+                }
+                Log.d("places", "LIKELY PLACES = " + likelyPlaces);
+                likelyPlaces.release();
             }
-        } else {
-            // If no resolution is available, display a dialog to the user with the error.
-            showErrorDialog(connectionResult.getErrorCode());
-        }
+        });
     }
 
-    /*
-     * Report location updates to the UI.
-     */
-    public void onLocationChanged(Location location) {
-        currentLocation = location;
-        if (lastLocation != null
-                && geoPointFromLocation(location)
-                .distanceInKilometersTo(geoPointFromLocation(lastLocation)) < 0.01) {
-            // If the location hasn't changed by more than 10 meters, ignore it.
-            return;
-        }
-        lastLocation = location;
-        LatLng myLatLng = new LatLng(location.getLatitude(), location.getLongitude());
 
-//        if (!hasSetUpInitialLocation) {
-//            // Zoom to the current location.
-//            updateZoom(myLatLng);
-//            hasSetUpInitialLocation = true;
-//        }
-//        // Update map radius indicator
-//        updateCircle(myLatLng);
-//        doMapQuery();
-//        doListQuery();
-    }
-
-    /*
-     * In response to a request to start updates, send a request to Location Services
-     */
-    private void startPeriodicUpdates() {
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            LocationServices.FusedLocationApi.requestLocationUpdates(
-                    locClient, locRequest, this);
-        }
-    }
-
-    /*
-     * In response to a request to stop updates, send a request to Location Services
-     */
-    private void stopPeriodicUpdates() {
-        locClient.disconnect();
-    }
-
-    /*
-     * Get the current location
-     */
-    private Location getLocation() {
-        // If Google Play Services is available
-        if (servicesConnected()) {
-            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                // Get the current location
-                return LocationServices.FusedLocationApi.getLastLocation(locClient);
-            }
-        }
-
-        return null;
-    }
-
-    /*
-  * Helper method to get the Parse GEO point representation of a location
-  */
-    private ParseGeoPoint geoPointFromLocation(Location loc) {
-        return new ParseGeoPoint(loc.getLatitude(), loc.getLongitude());
-    }
-
-    /*
+/*
  * Show a dialog returned by Google Play services for the connection error code
  */
     private void showErrorDialog(int errorCode) {
